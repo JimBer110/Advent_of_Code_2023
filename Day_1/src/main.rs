0e7677d469e5664b261f324cc6c7b129fb19d6fa
Jimmy Berglund <jimmy_berglund@hotmail.com>
2023-12-01 00:37:50 +0100

    Boilerplate for reading a file

--- Day_1/src/main.rs
+++ Day_1/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "input";
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let lines = contents.split("\n");
+
+    for (i, line) in lines.enumerate() {
+        println!("{}: {}", i, line)
+    }
+
+    std::process::exit(0);
 }
