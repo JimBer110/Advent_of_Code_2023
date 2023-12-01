dee47a927ae28daa9541a0bf341e89cb3a1fd5c7
Jimmy Berglund <jimmy_berglund@hotmail.com>
2023-12-01 19:27:13 +0100

    Progress, just commits so I can push to my desktop

--- Day_1/src/main.rs
+++ Day_1/src/main.rs
@@ -6,7 +6,13 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let lines = contents.split("\n");
 
+    // let mut data: Vec<(u16, u16)>;
+
     for (i, line) in lines.enumerate() {
+        for c in line.chars() {
+            println!("{}", c);
+        }
+        // data.push();
         println!("{}: {}", i, line)
     }
 
